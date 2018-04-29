92a47bf78805b9f89633fec7af2a36b5f067597f
bradenaa <bradenaltstatt@gmail.com>
2018-04-29 11:55:28 -0700

    DONE! with getting posts

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var rp = require("request-promise");
+var cherrio = require("cheerio");
+var axios = require("axios");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
